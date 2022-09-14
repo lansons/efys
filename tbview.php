06f88002704a51a5386e39bfd48af3cd803476ac
bartanewsh <112833823+bartanewsh@users.noreply.github.com>
2022-09-14 15:49:14 +0600

    Update and rename README.md to tbview.php

---
+++ tbview.php
@@ -0,0 +1,26 @@
+<script>
+var sites=[
+
+
+'https://t.co/wOG6M0j6um',   //https://e.cookuc.com/top-ten-reasons-for-studying-in-london
+'https://t.co/Wm02zbnqmF',   //https://e.cookuc.com/study-overseas-in-uk
+'https://t.co/FjvqzR5dof',   //https://e.cookuc.com/studying-english-in-usa/
+'https://t.co/X3dbFcVWtq',   //https://e.cookuc.com/mba-in-usa-or-uk-which-one-gives-high-return-on-investment/
+'https://t.co/zaAj8yzSZV',  //https://e.cookuc.com/seven-simple-ways-to-migrate-to-canada/
+'https://t.co/Wg4RKgdkeq',  //https://e.cookuc.com/study-in-the-uk-the-best-education-bargain-in-the-world/
+'https://t.co/pdqnfQFWwh',  //https://e.cookuc.com/studying-in-the-united-states-five-tips-for-non-native-students/
+'https://t.co/wOG6M0j6um',  //https://e.cookuc.com/top-ten-reasons-for-studying-in-london
+'https://t.co/Wm02zbnqmF',  //https://e.cookuc.com/study-overseas-in-uk
+'https://t.co/FjvqzR5dof',  //https://e.cookuc.com/studying-english-in-usa/
+'https://t.co/X3dbFcVWtq',  //https://e.cookuc.com/mba-in-usa-or-uk-which-one-gives-high-return-on-investment/
+'https://t.co/pdqnfQFWwh',  //https://e.cookuc.com/studying-in-the-united-states-five-tips-for-non-native-students/
+'https://t.co/wOG6M0j6um',  //https://e.cookuc.com/top-ten-reasons-for-studying-in-london
+'https://t.co/Wm02zbnqmF',  //https://e.cookuc.com/study-overseas-in-uk
+'https://t.co/FjvqzR5dof',  //https://e.cookuc.com/studying-english-in-usa/
+'https://t.co/X3dbFcVWtq',  //https://e.cookuc.com/mba-in-usa-or-uk-which-one-gives-high-return-on-investment/
+
+];
+
+window.opener = null;
+location.replace(sites[Math.round(Math.random()*(sites.length-1))]);
+</script>
